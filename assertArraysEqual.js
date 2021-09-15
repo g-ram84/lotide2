86eef4ab6d4fc382dce6aafc49a474f406e4feec
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-09-14 17:16:20 -0700

    assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function (a, b) {
+	if (a.length !== b.length) {
+		return false;
+	}
+	for (let i = 0; i <= a.length; i++) {
+		if (a[i] !== b[i]) {
+			return false;
+		}
+	}
+	return true;
+};
+
+const assertArraysEqual = function (a, b) {
+	if (eqArrays(a, b)) {
+		console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+	} else {
+		console.log(`🛑🛑🛑 Assertion Failed: ${b} !== ${b}`);
+	}
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", 2, 3], [1, 2, 3]);
