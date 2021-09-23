8908fad3e18eb30bb3d3a3dc92f6022a28510dfc
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-09-22 17:43:01 -0700

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function (a, b) {
-	if (a.length !== b.length) {
-		return false;
-	}
-	for (let i = 0; i <= a.length; i++) {
-		if (a[i] !== b[i]) {
-			return false;
-		}
-	}
-	return true;
-};
-
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = function (a, b) {
 	if (eqArrays(a, b)) {
 		console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
@@ -18,5 +7,4 @@ const assertArraysEqual = function (a, b) {
 	}
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["1", 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
