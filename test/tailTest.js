f92d68f4fd71082750b735cb3e8cd54bb5c34d48
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-09-22 17:37:36 -0700

    refactored tail test

---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const tail = require("../tail");
+const assertEqual = require("../assertEqual");
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
