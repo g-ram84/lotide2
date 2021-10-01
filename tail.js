d38fdc9bfcc54d179276d1e4e8eb3256a8640d92
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-10-01 15:21:46 -0700

    chai testing for middle and tail files

--- tail.js
+++ tail.js
@@ -1,9 +1,5 @@
 const tail = function (array) {
-	const newArr = [];
-	for (let i = 1; i < array.length; i++) {
-		newArr.push(array[i]);
-	}
-	return newArr;
+	return array.slice(1);
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+	it(`returns '[5, 6, 7]', from '[4, 5, 6, 7]`, () => {
+		assert.deepEqual(tail([4, 5, 6, 7]), [5, 6, 7]);
+	});
+	it(`returns '[2, 3]', from '[1, 2, 3]'`, () => {
+		assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+	});
+});
